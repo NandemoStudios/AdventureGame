1a471e3ffdc3dfc49e79f81dd8fe09dfc5d06109
JONATHAN_SWIFT <juniorjonswift@gmail.com>
2023-12-08 19:54:12 +0000

    Added the drawLine funcntion

--- main.py
+++ main.py
@@ -4,16 +4,11 @@ import draw
 import threading
 
 
-def drawTest():
-    newEngine.drawLoop()
-
-
 newWindow = window.Window("1280x720")
 
 pawn_list = [20, 20, 20, 20, 5], [20, 200, 200, 20, 5], [50, 50, 100, 100, 5]
 id_list = [1, 2, 3]
 newEngine = draw.Engine(newWindow, pawn_list, id_list)
-drawThread = threading.Thread(target=drawTest)
-drawThread.start()
+newEngine.drawLoop()
 
 newWindow.Start()
