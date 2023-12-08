3572007e23f7431fa2312fa737fa36fa484b121e
NandemoStudios <juniorjonswift@gmail.com>
2023-12-08 11:34:29 +0000

    draw_rectangle function added to the class, to allow for rectangles to be drawn.

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ class Window:
         self.root.geometry(self.geometry)
         self.create_canvas()
         self.draw_ellipse(20, 100, 5)
-        self.draw_ellipse(120, 200, 5)
+        self.draw_rectangle(100, 100, 20, 100, 5)
         self.root.mainloop()
 
     def create_canvas(self):
@@ -21,6 +21,10 @@ class Window:
     def draw_ellipse(self, x, y, line_width):
         circle = self.canvas.create_oval(x, y, y, x, width=line_width)
         return circle
+
+    def draw_rectangle(self, x, y, width, height, line_width):
+        rectangle = self.canvas.create_rectangle(x ,y, x+width, y+height, width=line_width)
+        return rectangle
 
 
 newWindow = Window("1280x720")
