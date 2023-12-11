4917abdc1d364d3a8eca5e3ceb01496705ce8f24
JONATHAN_SWYFTL <juniorjonswift@gmail>
2023-12-11 09:30:06 +0000

    Made a rendering thread that refreshes the screen

--- draw.py
+++ draw.py
@@ -9,7 +9,8 @@ class Engine:
 
     def drawLoop(self):
         for entry in range(0, len(self.pawn_list)):
-            # for each entry in the pawn-list, the game will find the correct ID, and draw it to the screen on set points
+            ''' for each entry in the pawn-list, the game will find the correct ID, and draw it to the screen on set 
+            points'''
             if self.id_list[entry] == 1:
                 self.root_window.draw_rectangle(self.pawn_list[entry][0],
                                                 self.pawn_list[entry][1],
--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 import window
 import draw
+import time
+import threading
+
+global shouldLoop
+
+targetFramerate = 30
+
+
+def RenderLoop():
+    while True:
+        newWindow.canvas.delete("all")
+        newEngine.drawLoop()
+        time.sleep(1/targetFramerate)
 
 
 newWindow = window.Window("1280x720")
@@ -7,6 +20,8 @@ newWindow = window.Window("1280x720")
 pawn_list = [20, 20, 20, 20, 5], [20, 200, 200, 20, 5], [50, 50, 100, 100, 5]
 id_list = [1, 2, 3]
 newEngine = draw.Engine(newWindow, pawn_list, id_list)
-newEngine.drawLoop()
+
+renderingThread = threading.Thread(target=RenderLoop, daemon=True)
+renderingThread.start()
 
 newWindow.Start()
